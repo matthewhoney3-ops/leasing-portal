cd9903880450c4410f8e46b39ac18d6703e657ce
Matthew Honey <matthew.honey3@gmail.com>
2026-06-22 16:49:18 -0700

    Bigger logo, links to main site

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -1,13 +1,15 @@
 import logoFull from '../assets/logo-full.png'
 
 export default function BrandMark({ size = 'md' }) {
-  const heights = { sm: 32, md: 48, lg: 64 }
+  const heights = { sm: 40, md: 64, lg: 80 }
   const h = heights[size] ?? heights.md
   return (
-    <img
-      src={logoFull}
-      alt="Golden Hive Capital"
-      style={{ height: h, width: 'auto' }}
-    />
+    <a href="https://goldenhivecapital.com" target="_blank" rel="noreferrer">
+      <img
+        src={logoFull}
+        alt="Golden Hive Capital"
+        style={{ height: h, width: 'auto' }}
+      />
+    </a>
   )
 }
